289a7f79385ee89302a81b6b2145afff2d803edf
lizhangwei <534432591@qq.com>
2019-12-18 13:43:51 +0800

    first commit

---
+++ src/main/java/com/example/tiktok/pojo/User.java
@@ -0,0 +1,45 @@
+package com.example.tiktok.pojo;
+
+public class User {
+
+  private String id;
+  private String name;
+  private long phonenum;
+  private String password;
+
+  public String getId() {
+    return id;
+  }
+
+  public void setId(String id) {
+    this.id = id;
+  }
+
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+
+  public long getPhonenum() {
+    return phonenum;
+  }
+
+  public void setPhonenum(long phonenum) {
+    this.phonenum = phonenum;
+  }
+
+
+  public String getPassword() {
+    return password;
+  }
+
+  public void setPassword(String password) {
+    this.password = password;
+  }
+
+}
